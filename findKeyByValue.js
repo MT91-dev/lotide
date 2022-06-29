52c7f3aa0d3a042f9f01554e3ef5c9276b9f46a2
MT91-dev <tariqm1991@gmail.com>
2022-06-29 23:31:16 +0000

    Created function that finds the key by a value

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+//Function below takes two parameters and checks whether they are the same. Provides with an assertion output based on the two parameters.
+const assertEqual = function (actual, expected) {
+  //checks whether the two parameters are the same and outputs a console statement using template literal
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else { //has a default case to the if statement for when the two parameters are not the same, outputs a console statement using template literal
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (object, value) => {
+  const objKeys = Object.keys(object);
+  for (const element of objKeys) {
+    if (object[element] === value) {
+      return element;
+    }
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
