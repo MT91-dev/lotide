b92716c1b79db339490c5dfa3eb040fb47e4fec8
MT91-dev <tariqm1991@gmail.com>
2022-06-29 00:53:06 +0000

    Created function that compares two arrays for a perfect match

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (array1, array2) => {
+  let perfectMatch = true;
+  
+  if (array1.length !== array2.length) {
+    perfectMatch = false;
+  }
+  for (let index = 0; index < array1.length; index++) {
+    if (array1[index] !== array2[index]){
+      perfectMatch = false;
+    }
+  }
+  return perfectMatch;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
