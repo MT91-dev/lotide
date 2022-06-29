3991b106b1a79fb56f54b07dc16de67af7a1ebcc
MT91-dev <tariqm1991@gmail.com>
2022-06-29 00:51:34 +0000

    Added a function that returns the tail of an array

--- tail.js
+++ tail.js
@@ -23,6 +23,5 @@ const tail = (arr) => {
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
