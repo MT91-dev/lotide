d106306c4fe6d08bdaeb05769ab975214ab81349
MT91-dev <tariqm1991@gmail.com>
2022-06-29 00:50:45 +0000

    Developed the assertEqual function for checking assertion on arrays.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,13 @@
 const assertArraysEqual = (array1, array2) => {
   let failedIndex;
   let perfectMatch = true;
-  
   const eqArrays = (array1, array2) => {
-  
+
     if (array1.length !== array2.length) {
       perfectMatch = false;
     }
     for (let index = 0; index < array1.length; index++) {
-      if (array1[index] !== array2[index]){
+      if (array1[index] !== array2[index]) {
         perfectMatch = false;
         failedIndex = index;
       }
@@ -17,9 +16,7 @@ const assertArraysEqual = (array1, array2) => {
     // console.log(perfectMatch);
     // console.log(failedIndex);
   }
-
   eqArrays(array1, array2);
-  
   if (perfectMatch) {
     console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
