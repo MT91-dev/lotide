6152fa2690c9f728eb42a332fbfafd44b9ed7c2a
MT91-dev <tariqm1991@gmail.com>
2022-07-04 20:33:56 +0000

    Updated Tail function with export and tested with require

--- tail.js
+++ tail.js
@@ -1,16 +1,21 @@
 //Function below takes two parameters and checks whether they are the same. Provides with an assertion output based on the two parameters.
-const assertEqual = function (actual, expected) {
-  return (actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`));
-};
+// const assertEqual = function (actual, expected) {
+//   return (actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`));
+// };
+
+// const assertEqual = require("./assertEqual.js")
 
 //Function below takes an array as variable arr and returns the tail of the array
+
 const tail = (arr) => {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-const check = ["Do", "A", "4", "Loop"]
-console.log(tail (check));
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(check.length, 4); // original array should still have 3 elements!
+module.exports = tail;
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// const check = ["Do", "A", "4", "Loop"]
+// console.log(tail (check));
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(check.length, 4); // original array should still have 3 elements!
