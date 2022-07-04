e76d8cc07f3a9d8cb85b6540d7ff18a30c39a3c1
MT91-dev <tariqm1991@gmail.com>
2022-07-04 22:19:10 +0000

    Main file that requires all functions, adds them to object and exports the object

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const without = require("./without");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+const eqObjects = require("./eqObjects");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const letterPositions = require("./letterPositions");
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  without,
+  map,
+  takeUntil,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual,
+  eqObjects,
+  assertObjectsEqual,
+  findKey,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
+};
